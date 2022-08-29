2635b8b38039948f16cd9ff0dad1bf2c12e9f62a
BarbaraGuimaraes21 <bahcamargo21@gmail.com>
2022-08-29 20:37:12 -0300

    importação de componentes e react-router-dom

--- venture-labs/src/App.jsx
+++ venture-labs/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Menu from './components/Menu/Menu'
+import Home from "./Pages/Home/Home"
+import Cadastro from "./Pages/Cadastro/Cadastro"
+import S from "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className={S.divPrincipal}>
+        <Menu/>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </BrowserRouter>
   )
 }
 
